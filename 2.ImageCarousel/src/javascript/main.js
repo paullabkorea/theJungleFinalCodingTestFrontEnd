1415fa6793dd9230afee0139e6f62d8db1dc6000
zeenee <dbwls982@gmail.com>
2021-04-22 16:35:32 +0900

    js 주석

--- 2.ImageCarousel/src/javascript/main.js
+++ 2.ImageCarousel/src/javascript/main.js
@@ -1,7 +1,77 @@
+// 엘리먼트 가져오기
 const carouselUl = document.querySelector(".carousel__list");
+const imageInput = document.querySelector("#image-upload__input");
 const prevButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
 
+// 이미지 위치 변경
+function changeTransform(){
+    let items = document.querySelectorAll(".carousel__item");
+
+    items.forEach((e, i) => {
+        let degree = 360/items.length;
+        if(items.length > 1) {
+            if(i == 0) {
+                e.style.transform = "rotateY(0deg) translateZ(250px)";
+            } else {
+                e.style.transform = `rotateY(${degree*i}deg) translateZ(250px) rotateY(-${degree*i}deg)`;
+            }
+        }
+        if(items.length >= 5) {
+            e.style.webkitBoxReflect = "below 20px linear-gradient(transparent 45%, rgba(255, 255, 255, 0.25))";
+            if(i == 0) {
+                e.style.transform = "rotateY(0deg) translateZ(250px)";
+            } else if(i == 1) {
+                e.style.transform = `rotateY(72deg) translateZ(250px) rotateY(-72deg)`;
+            } else if(i == 2) {
+                e.style.transform = `rotateY(144deg) translateZ(250px) rotateY(-144deg) translateX(400px)`;
+            } else if(i == items.length-2) {
+                e.style.transform = `rotateY(216deg) translateZ(250px) rotateY(-216deg) translateX(-400px)`;
+            } else if(i == items.length-1) {
+                e.style.transform = `rotateY(288deg) translateZ(250px) rotateY(-288deg)`;
+            } else {
+                e.style.transform = `rotateY(${degree*i}deg) translateZ(250px) rotateY(-${degree*i}deg)`;
+                e.style.webkitBoxReflect = "below 20px linear-gradient(transparent, transparent)";
+            }
+        }
+    });
+}
+
+// 이미지 태그 생성
+function createTag(url) {
+    let list = document.createElement("li");
+    let img = document.createElement("img");
+    list.setAttribute("class", "carousel__item");
+    img.src = url;
+    list.appendChild(img);
+    let items = document.querySelectorAll(".carousel__item");
+
+    if(items.length < 1) {
+        list.classList.add("now");
+    }
+
+    return list;
+}
+
+// 이미지 업로드
+function uploadImg(value) {
+    if(value.files) {
+        let reader = new FileReader();
+
+        reader.onload = e => {
+            let imgUrl = e.target.result;
+            carouselUl.appendChild(createTag(imgUrl));
+            changeTransform();
+        };
+        reader.readAsDataURL(value.files[0]);
+    }
+}
+
+imageInput.addEventListener("change", e => {
+    uploadImg(e.target);
+});
+
+// Next, Prev 이동 버튼
 function moveNext(){
     let items = document.querySelectorAll(".carousel__item");
 
@@ -51,73 +121,7 @@ function movePrev(){
 nextButton.addEventListener("click", moveNext);
 prevButton.addEventListener("click", movePrev);
 
-function changeTransform(){
-    let items = document.querySelectorAll(".carousel__item");
-
-    items.forEach((e, i) => {
-        let degree = 360/items.length;
-        if(items.length > 1) {
-            if(i == 0) {
-                e.style.transform = "rotateY(0deg) translateZ(250px)";
-            } else {
-                e.style.transform = `rotateY(${degree*i}deg) translateZ(250px) rotateY(-${degree*i}deg)`;
-            }
-        }
-        if(items.length >= 5) {
-            e.style.webkitBoxReflect = "below 20px linear-gradient(transparent 45%, rgba(255, 255, 255, 0.25))";
-            if(i == 0) {
-                e.style.transform = "rotateY(0deg) translateZ(250px)";
-            } else if(i == 1) {
-                e.style.transform = `rotateY(72deg) translateZ(250px) rotateY(-72deg)`;
-            } else if(i == 2) {
-                e.style.transform = `rotateY(144deg) translateZ(250px) rotateY(-144deg) translateX(400px)`;
-            } else if(i == items.length-2) {
-                e.style.transform = `rotateY(216deg) translateZ(250px) rotateY(-216deg) translateX(-400px)`;
-            } else if(i == items.length-1) {
-                e.style.transform = `rotateY(288deg) translateZ(250px) rotateY(-288deg)`;
-            } else {
-                e.style.transform = `rotateY(${degree*i}deg) translateZ(250px) rotateY(-${degree*i}deg)`;
-                e.style.webkitBoxReflect = "below 20px linear-gradient(transparent, transparent)";
-            }
-        }
-    });
-}
-
-// 이미지 로드 후 삽입
-function createTag(url) {
-    let list = document.createElement("li");
-    let img = document.createElement("img");
-    list.setAttribute("class", "carousel__item");
-    img.src = url;
-    list.appendChild(img);
-    let items = document.querySelectorAll(".carousel__item");
-
-    if(items.length < 1) {
-        list.classList.add("now");
-    }
-
-    return list;
-}
-
-function uploadImg(value) {
-    if(value.files) {
-        let reader = new FileReader();
-
-        reader.onload = e => {
-            let imgUrl = e.target.result;
-            carouselUl.appendChild(createTag(imgUrl));
-            changeTransform();
-        };
-        reader.readAsDataURL(value.files[0]);
-    }
-}
-
-const imageInput = document.querySelector("#image-upload__input");
-
-imageInput.addEventListener("change", e => {
-    uploadImg(e.target);
-});
-
+// 페이지 로드 > 이미지 위치 변경
 window.onload = function() {
     changeTransform();
 }
