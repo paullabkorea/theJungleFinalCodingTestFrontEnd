c53a68ec56baafb3e947b3bfb5ed774c3a4a76c2
winterguard <aliasbonfire@gmail.com>
2021-06-15 23:24:39 +0900

    뮤직 리스트 css

---
+++ 4.MusicPlayer/src/javascripts/components/SearchView/index.js
@@ -0,0 +1,22 @@
+class SearchView {
+    constructor() {
+
+    }
+
+    static createRenderElement() {
+        return `
+            <article class="contents-search">
+                <div class="search-controller">
+                    <input class="search-input" type="text" placeholder="검색"/>
+                    <button class="search-button">
+                        <i class="icon-search-controller"></i>
+                    </button>
+                </div>
+            </article>
+        `
+    }
+
+    render() {
+
+    }
+}
