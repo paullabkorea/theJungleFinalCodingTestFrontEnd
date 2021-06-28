1fe938fcd43d62eeafe734b2e6d3f084627bb43a
nugurejeil <nugurejeil@ucsit.co.kr>
2021-06-29 00:59:43 +0900

    2강 주석 추가

--- 2.ImageCarousel/cypress/integration/b.image_carousel_javascript.spec.js
+++ 2.ImageCarousel/cypress/integration/b.image_carousel_javascript.spec.js
@@ -1,13 +1,19 @@
+// reference types="cypress" = 사이프러스 라이브러리 autocomplete 
+///<reference types="cypress"/>
 describe('ImageCarousel js', () => {
+  // it 을 통한 테스트 블록 생성, 첫번째 인자로 테스트 제목. 
   it('Next 버튼이 정상적으로 동작하는지 확인합니다.', () => {
+    // cy : 사이프러스 객체. 모든 사이프러스 메서드를 사용할수 있게 함.
+    // visit : 해당 url에 접속 
     cy.visit("http://localhost:3000");
 
     const images = [];
-
+    // get : dom 셀렉터 
     cy.get('.carousel__list img').each(($img) => {
       images.push($img);
     }).then(() => {
       const nextButton = cy.get('button.next-btn');
+      // click(): 클릭 액션
       nextButton.click();
 
       cy.get('.carousel__list img').each(($img, index) => {
